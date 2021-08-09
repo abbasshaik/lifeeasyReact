ed198b128b28f14f8d430c31bcf18223ea05e45c
abbasshaik <77068394+abbasshaik@users.noreply.github.com>
2021-08-09 20:17:16 +0530

    Updates

---
+++ precache-manifest.123f023dfb1abbcba953ba5c858417fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1d393c3c4098b6d467b",
+    "url": "/static/css/main.7cd624cd.chunk.css"
+  },
+  {
+    "revision": "d1d393c3c4098b6d467b",
+    "url": "/static/js/main.045aa854.chunk.js"
+  },
+  {
+    "revision": "604e102db3fa5f24819c",
+    "url": "/static/js/runtime~main.a4b62eed.js"
+  },
+  {
+    "revision": "b346a6fc79dca845dbfa",
+    "url": "/static/js/2.4fa942c9.chunk.js"
+  },
+  {
+    "revision": "ee609f01ae985e2ba079",
+    "url": "/static/js/3.af959e76.chunk.js"
+  },
+  {
+    "revision": "b62dd340de993f1ee966a220c72b21ee",
+    "url": "/index.html"
+  }
+];
