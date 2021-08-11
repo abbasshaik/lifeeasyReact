32efb1fccaa7170ee017b6d7d2fbf780b8a15b5e
abbasshaik <77068394+abbasshaik@users.noreply.github.com>
2021-08-11 20:03:54 +0530

    Animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import React from 'react';
-import UploadS3 from './upload';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Navbar'
+import "./App.css";
+import React from "react";
+import UploadS3 from "./upload";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Navbar";
+import SimpleTabs from "./Tab";
 function App() {
   return (
-  <React.Fragment>
-    < NavBar/>
-    < UploadS3/>
-  </React.Fragment>
+    <React.Fragment>
+      <NavBar />
+      <SimpleTabs />
+    </React.Fragment>
   );
 }
 
